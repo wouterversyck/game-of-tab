9cad5d9df26c5cebdf45d5aee9739978a1350ed8
wouterversyck <wouter.versyck@gmail.com>
2020-08-23 12:08:49 +0200

    Add phaser and init game

---
+++ src/components/game-of-life/GameOfLife.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="Game__game-container"></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+import Game from './Game';
+
+@Component
+export default class HelloWorld extends Vue {
+  mounted() {
+    browser.runtime.sendMessage({});
+    new Game();
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 20px;
+}
+</style>
--- src/override/App.vue
+++ src/override/App.vue
@@ -1,14 +1,18 @@
 <template>
-  <hello-world />
+  <div>
+    <p>The game of life biiitch!</p>
+    <game-of-life />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue';
+import GameOfLife from '@/components/game-of-life/GameOfLife.vue';
 import { Component } from 'vue-property-decorator';
 
 @Component({
-  components: { HelloWorld }
+  components: { HelloWorld, GameOfLife }
 })
 export default class App extends Vue {}
 </script>
